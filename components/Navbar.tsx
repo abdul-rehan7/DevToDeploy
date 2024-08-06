47aaa2bdbb41695a632d113ab0f5811d7a1a792d
rehanmirza76 <abdulrehan0317@gmail.com>
2024-08-06 22:12:57 +0500

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="bg-[#000000c7] items-center z-50 fixed flex justify-between text-lg lg:text-2xl w-full text-white py-4 px-5 lg:px-12">
+    <div className="bg-[#000000d7] items-center z-50 fixed flex justify-between text-base lg:text-2xl w-full text-white py-3 lg:py-4 px-2 lg:px-12">
       <div className="logo font-bold">DevToDeploy</div>
-      <div className="nav-links space-x-7">
+      <div className="nav-links space-x-4 lg:space-x-7">
         <Link className="link" href="/">
           Home
         </Link>
@@ -18,7 +18,7 @@ const Navbar = () => {
         <Link className="link" href="/contact">
           Contact
         </Link>
-        <button  className="bg-transparent active:bg-[#292929] text-white border-[1px] border-white rounded-sm text-xl lg:text-xl  py-2 px-4">
+        <button  className="bg-transparent active:bg-[#292929] text-white border-[1px] border-white rounded-sm text-base lg:text-xl  py-1 px-2 lg:py-2 lg:px-4">
           <a href="https://www.linkedin.com/" target="_blank">Connect</a>
         </button>
       </div>
