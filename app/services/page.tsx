a90c8bbc5686084eda4e6dbd712c17cac0d805b4
rehanmirza76 <abdulrehan0317@gmail.com>
2024-08-08 01:55:38 +0500

    update

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,17 +1,173 @@
-import Hero from '@/components/Hero'
-import React from 'react'
-import {Cards}  from '@/components/Services'
+import Hero from "@/components/Hero";
+import React from "react";
+import { TbDeviceDesktopCode } from "react-icons/tb";
 
 const Services = () => {
   return (
-    <div className='bg-[var(--primary)] flex flex-col items-center'>
-      <Hero  title='Our Services' description='We offer a range of professional services designed to meet your needs. From custom development and design to expert consulting, our services are crafted to deliver results and exceed expectations.'/>
-    <div className="heading py-7">
-      What we Offer
+    <div className="bg-[var(--primary)] flex flex-col items-center">
+      <Hero title="We Believe In Code " description="" />
+      {/* I am Service */}
+      <div className="flex flex-col">
+        <div className="flex lg:flex-row flex-col  space-y-4 justify-center items-center px-5  md:px-[7rem] py-10 text-left bg-[var(--third)] w-screen md:h-[70vh] ">
+          <div className="first text-[11rem]">
+            <TbDeviceDesktopCode className="lg:mr-16 text-[var(--secondary)]" />
+          </div>
+          <div className="second flex flex-col space-y-6">
+            <div className="heading__2">SERVICES</div>
+            <div className="para__2">
+              In this digital age, if your business is not online, you don’t
+              exist. So if you want to create a new website for your business or
+              revamp an existing one, Olive Digital is at your service. Our web
+              solutions and designs are search engine friendly, aesthetically
+              appealing, interactive, and powerful enough to build a solid
+              presence of your business in the online world. We have the
+              expertise, skills and experience to design and deliver complex
+              systems, custom web platforms, and mobile applications. What makes
+              us different from others is our working methodology; we understand
+              your needs, focus on your requirements, and deliver beyond your
+              expectations. At present, we are offering following design and
+              development services
+            </div>
+            <ul className="para__2 list-inside ">
+              <li>• Corporate ID</li>
+              <li>• Website design & development</li>
+              <li>• CMS & web dashboard development</li>
+              <li>• Responsive & mobile website development</li>
+              <li>• CMS & web dashboard development</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      {/* I am Service */}
+      <div className="flex flex-col">
+        <div className="flex lg:flex-row flex-col space-y-4 justify-center items-center px-5  md:px-[7rem] py-10 text-left bg-[var(--primary)] w-screen md:h-[70vh] ">
+          <div className="second flex flex-col space-y-6">
+            <div className="heading__2">SERVICES</div>
+            <div className="para__2">
+              In this digital age, if your business is not online, you don’t
+              exist. So if you want to create a new website for your business or
+              revamp an existing one, Olive Digital is at your service. Our web
+              solutions and designs are search engine friendly, aesthetically
+              appealing, interactive, and powerful enough to build a solid
+              presence of your business in the online world. We have the
+              expertise, skills and experience to design and deliver complex
+              systems, custom web platforms, and mobile applications. What makes
+              us different from others is our working methodology; we understand
+              your needs, focus on your requirements, and deliver beyond your
+              expectations. At present, we are offering following design and
+              development services
+            </div>
+            <ul className="para__2 list-inside ">
+              <li>• Corporate ID</li>
+              <li>• Website design & development</li>
+              <li>• CMS & web dashboard development</li>
+              <li>• Responsive & mobile website development</li>
+              <li>• CMS & web dashboard development</li>
+            </ul>
+          </div>
+          <div className="first text-[11rem]">
+            <TbDeviceDesktopCode className="lg:ml-16 text-[var(--secondary)]" />
+          </div>
+        </div>
+      </div>
+      {/* I am Service */}
+      <div className="flex flex-col">
+        <div className="flex lg:flex-row flex-col space-y-4 justify-center items-center px-5  md:px-[7rem] py-10 text-left bg-[var(--third)] w-screen md:h-[70vh] ">
+          <div className="first text-[11rem]">
+            <TbDeviceDesktopCode className="lg:mr-16 text-[var(--secondary)]" />
+          </div>
+          <div className="second flex flex-col space-y-6">
+            <div className="heading__2">SERVICES</div>
+            <div className="para__2">
+              In this digital age, if your business is not online, you don’t
+              exist. So if you want to create a new website for your business or
+              revamp an existing one, Olive Digital is at your service. Our web
+              solutions and designs are search engine friendly, aesthetically
+              appealing, interactive, and powerful enough to build a solid
+              presence of your business in the online world. We have the
+              expertise, skills and experience to design and deliver complex
+              systems, custom web platforms, and mobile applications. What makes
+              us different from others is our working methodology; we understand
+              your needs, focus on your requirements, and deliver beyond your
+              expectations. At present, we are offering following design and
+              development services
+            </div>
+            <ul className="para__2 list-inside ">
+              <li>• Corporate ID</li>
+              <li>• Website design & development</li>
+              <li>• CMS & web dashboard development</li>
+              <li>• Responsive & mobile website development</li>
+              <li>• CMS & web dashboard development</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      {/* I am Service */}
+      <div className="flex flex-col">
+        <div className="flex lg:flex-row flex-col space-y-4 justify-center items-center px-5  md:px-[7rem] py-10 text-left bg-[var(--primary)] w-screen md:h-[70vh] ">
+          <div className="second flex flex-col space-y-6">
+            <div className="heading__2">SERVICES</div>
+            <div className="para__2">
+              In this digital age, if your business is not online, you don’t
+              exist. So if you want to create a new website for your business or
+              revamp an existing one, Olive Digital is at your service. Our web
+              solutions and designs are search engine friendly, aesthetically
+              appealing, interactive, and powerful enough to build a solid
+              presence of your business in the online world. We have the
+              expertise, skills and experience to design and deliver complex
+              systems, custom web platforms, and mobile applications. What makes
+              us different from others is our working methodology; we understand
+              your needs, focus on your requirements, and deliver beyond your
+              expectations. At present, we are offering following design and
+              development services
+            </div>
+            <ul className="para__2 list-inside ">
+              <li>• Corporate ID</li>
+              <li>• Website design & development</li>
+              <li>• CMS & web dashboard development</li>
+              <li>• Responsive & mobile website development</li>
+              <li>• CMS & web dashboard development</li>
+            </ul>
+          </div>
+          <div className="first text-[11rem]">
+            <TbDeviceDesktopCode className="lg:ml-16 text-[var(--secondary)]" />
+          </div>
+        </div>
+      </div>
+      {/* I am Service */}
+      <div className="flex flex-col">
+        <div className="flex lg:flex-row flex-col space-y-4 justify-center items-center px-5  md:px-[7rem] py-10 text-left bg-[var(--third)] w-screen md:h-[70vh] ">
+          <div className="first text-[11rem]">
+            <TbDeviceDesktopCode className="lg:mr-16 text-[var(--secondary)]" />
+          </div>
+          <div className="second flex flex-col space-y-6">
+            <div className="heading__2">SERVICES</div>
+            <div className="para__2">
+              In this digital age, if your business is not online, you don’t
+              exist. So if you want to create a new website for your business or
+              revamp an existing one, Olive Digital is at your service. Our web
+              solutions and designs are search engine friendly, aesthetically
+              appealing, interactive, and powerful enough to build a solid
+              presence of your business in the online world. We have the
+              expertise, skills and experience to design and deliver complex
+              systems, custom web platforms, and mobile applications. What makes
+              us different from others is our working methodology; we understand
+              your needs, focus on your requirements, and deliver beyond your
+              expectations. At present, we are offering following design and
+              development services
+            </div>
+            <ul className="para__2 list-inside ">
+              <li>• Corporate ID</li>
+              <li>• Website design & development</li>
+              <li>• CMS & web dashboard development</li>
+              <li>• Responsive & mobile website development</li>
+              <li>• CMS & web dashboard development </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-      <Cards/>
-    </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ interface HeroProps {
 
 const Hero: React.FC<HeroProps> = ({ title, description }) => {
   return (
-    <section className="flex space-y-7 flex-col lg:flex-row justify-center items-center px-20 h-[80vh] lg:h-screen w-screen bg-[var(--primary)]">
+    <section className="flex space-y-7 flex-col lg:flex-row justify-center items-center px-20 h-[80vh] lg:h-[70vh] w-screen bg-[var(--primary)]">
       <div className="text-center flex space-y-5 flex-col text-[var(--secondary)]  lg:w-[70vw] w-[90vw] ">
         <h1 className="heading">{title}</h1>
         <p className="para">{description}</p>
