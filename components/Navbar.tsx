dd6328dc543be518530d5f98e3e3aa03556a4216
rehanmirza76 <abdulrehan0317@gmail.com>
2024-08-10 20:05:46 +0500

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="bg-[var(--thirdcolor)]  items-center z-50  flex justify-between text-xs lg:text-2xl w-full text-[var(--secondary)] py-3 lg:py-4 px-2 lg:px-12">
+    <div className="bg-[var(--thirdcolor)]  items-center z-50  flex justify-between text-[0.6rem] lg:text-2xl w-full text-[var(--secondary)] py-3 lg:py-4 px-2 lg:px-12">
       <div className="logo ">
         <img src="/logo-trans.png"  className="lg:w-[10rem] lg:h-[4rem] w-[6rem] h-[2.5rem]" alt="" />
       </div>
@@ -41,14 +41,14 @@ const Navbar = () => {
         <Link className="link" href="/contact">
           Contact
         </Link>
-        <button className="bg-transparent text-[var(--secondary)] border-[1px] border-[var(--secondary)] rounded-sm text-xs lg:text-xl  py-1 px-2 lg:py-2 lg:px-4">
+        <button className="bg-transparent text-[var(--secondary)] border-[1px] border-[var(--secondary)] rounded-sm text-[0.6rem] lg:text-xl  py-1 px-2 lg:py-2 lg:px-4">
           <a href="https://www.linkedin.com/" target="_blank">
             Connect
           </a>
         </button>
         <button
           onClick={toggleTheme}
-          className="  bg-transparent text-[var(--secondary)] text-base lg:text-xl  py-1 px-2 lg:py-2 lg:px-4"
+          className="  bg-transparent text-[var(--secondary)] text-xs lg:text-xl  py-1 px-2 lg:py-2 lg:px-4"
         >
           {theme.sunVisible ? <FaSun id="sun" /> : <FaMoon id="moon" />}
         </button>
